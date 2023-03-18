e4c7141175fd4a06de40f715b3b0040c02275bd9
Rafa <rafaginard@gmail.com>
2023-03-18 22:14:26 +0100

    Se añade font Karla

---
+++ src/components/fonts.jsx
@@ -0,0 +1,10 @@
+import { Global } from "@emotion/react"
+
+const Fonts = () => (
+  <Global
+    styles={`
+    @import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,300;0,400;0,700;1,700&display=swap');
+      `}
+    />
+)
+  export default Fonts
